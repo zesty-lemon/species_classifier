844bc6a0086665af7d15fbf153089d28ef5f3e95
Giles <51659929+zesty-lemon@users.noreply.github.com>
2026-04-09 12:17:40 -0400

    finish dataset statistics

--- scripts/dataset_statistics_graphs.py
+++ scripts/dataset_statistics_graphs.py
@@ -1,4 +1,5 @@
 import os
+import statistics
 from pathlib import Path
 
 import torchvision
@@ -17,7 +18,7 @@ and then saves a report to the /graphs_and_stats directory
 CURRENT_DATASET_NAME = c.FULL_DATASET
 DATA_PATH = str(Path(__file__).resolve().parent.parent.parent / "data")
 REPORT_DIRECTORY = str(Path(__file__).resolve().parent.parent / "graphs_and_stats")
-
+TOO_FEW_THRESHOLD = 10
 # ------------ Load Data ------------
 print("------ Begin Loading Data ------")
 
@@ -59,19 +60,28 @@ for label, indices in class_to_indices.items():
     num_samples_in_class = int(len(indices))
     print(f"Class {label} ({class_name}): {num_samples_in_class} samples")
     total_num_samples = total_num_samples + num_samples_in_class
-    sample_counts.append(total_num_samples)
+    sample_counts.append(num_samples_in_class)
 
 report_path = os.path.join(REPORT_DIRECTORY, f"{CURRENT_DATASET_NAME}_dataset_report.txt")
+
+# Calculate Additional Statistics
+num_below_threshold = 0
+for species_count in sample_counts:
+    if species_count < TOO_FEW_THRESHOLD:
+        num_below_threshold = num_below_threshold + 1
 
 # Build & Save Final Report
 with open(report_path, "w", encoding="utf-8") as f:
     f.write(f"----- {CURRENT_DATASET_NAME} Dataset Report -----\n")
     f.write("=================================\n\n")
 
-    f.write("-------- General Metrics -------\n")
+    f.write("-------- General Metrics --------\n")
     f.write(f"Number of Plant Classes: {int(num_plant_classes)}\n")
     f.write(f"Average Samples Per Class: {(total_num_samples/num_plant_classes):.1f}\n")
-
+    f.write(f"Median Samples Per Class: {int(statistics.median(sample_counts))}\n")
+    f.write(f"Min Samples Per Class: {int(min(sample_counts))}\n")
+    f.write(f"Max Samples Per Class: {int(max(sample_counts))}\n")
+    f.write(f"Classes with fewer than {TOO_FEW_THRESHOLD} examples: {int(num_below_threshold)} out of {int(num_plant_classes)}\n")
     f.write("\n")
 
 print(f"Saved model report to: {report_path}")
@@ -79,17 +89,6 @@ print(f"Saved model report to: {report_path}")
 # ------------ Visualizations ------------
 # Get per-class counts sorted in descending order
 sorted_counts = sorted([len(indices) for indices in class_to_indices.values()], reverse=True)
-
-sorted_sample_counts = sample_counts
-sorted_sample_counts.sort()
-
-plt.hist(sorted_sample_counts, bins=len(sorted_sample_counts), color='black', edgecolor='black')
-
-# 3. Add labels and show the plot
-plt.title("Class Distribution")
-plt.xlabel("Class Number")
-plt.ylabel("Frequency")
-plt.show()
 
 # --- Sorted Bar Chart ---
 fig, ax = plt.subplots(figsize=(14, 5))
