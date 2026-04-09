64782be0b11658433ce74a455060c90f3ed40274
Giles <51659929+zesty-lemon@users.noreply.github.com>
2026-04-09 10:41:04 -0400

    add dataset_utils

--- scripts/dataset_utils.py
+++ scripts/dataset_utils.py
@@ -210,6 +210,37 @@ def read_image_annotations_from_file(annotation_filepath: str = "data/2021_train
 
     return coords
 
+
+# Filter Dataset by Kingdom (Delete Directories Not In Desired Kingdom)
+def remove_unwanted_kingdoms(kingdom_to_keep: str, data_filepath: str = '/data/2021_train_mini'):
+    print(f"----- BEGIN Filtering By Kingdom = {kingdom_to_keep} -----")
+    PROJECT_ROOT = Path(__file__).resolve().parent.parent
+    DATA_DIR = PROJECT_ROOT / data_filepath
+
+    directory_path = Path(DATA_DIR)
+
+    # Collect directories for tqdm knows the total
+    subdirs = [p for p in directory_path.iterdir() if p.is_dir()]
+
+    num_directories_kept = 0
+    num_directories_deleted = 0
+
+    for path in tqdm(subdirs, desc="Filtering directories", unit="dir"):
+        if kingdom_to_keep not in str(path):
+            try:
+                shutil.rmtree(path)
+                num_directories_deleted += 1
+            except OSError as e:
+                print(f"Error: {path} : {e.strerror}")
+        else:
+            num_directories_kept += 1
+
+    print("----- Filtering Job Report -----")
+    print(f"Directories Kept:    {num_directories_kept}")
+    print(f"Directories Deleted: {num_directories_deleted}")
+    print(f"----- END Filtering By Kingdom = {kingdom_to_keep} -----")
+
+
 if __name__ == "__main__":
     read_image_annotations_from_file()
     print(is_in_vermont( 40.7128, -74.0060))
