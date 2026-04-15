d9941525f3f0f8311ffd5b4ae0cee0b628c2246e
Giles <51659929+zesty-lemon@users.noreply.github.com>
2026-04-15 13:07:32 -0400

    another refactor

--- models/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-This directory contains the setup code to define various models
-
-It also contains the utility code to evaluate those models and generate reports and graphs
--- models/model_evaluators/resnet_50_scratch_trained.py
+++ models/model_evaluators/resnet_50_scratch_trained.py
@@ -1,12 +1,16 @@
 from config import constants as c
 from config.device_config import device, device_name
 import utils.data_load_and_config_util as data_config
-from models.model_definitions import resnet_50
-from utils import evaluate_utils, train_utils, model_utils
+from model_definitions import resnet_50_scratch_trained
+from utils import evaluate_utils
+from models.model_utils import model_utils, train_utils
 
+# ------------ Initial Configuration ------------
+MODEL_NAME = "ResNet50 Scratch Trained"
+DATASET_USED = c.FULL_DATASET
 
 # ------------ Dataset Setup ------------
-CURRENT_DATASET_NAME, DATA_PATH = data_config.get_dataset_name_and_path(c.MINI_DATASET)
+CURRENT_DATASET_NAME, DATA_PATH = data_config.get_dataset_name_and_path(DATASET_USED)
 
 # ------------ Initialize Loaders ------------
 train_loader, val_loader, num_plant_classes = data_config.load_vermont_plant_data(dataset_name=CURRENT_DATASET_NAME,
@@ -15,8 +19,7 @@ train_loader, val_loader, num_plant_classes = data_config.load_vermont_plant_dat
                                                                                   batch_size=128)
 
 # ------------ Initialize Model ------------
-MODEL_NAME = "ResNet50 Scratch Trained"
-resnet50_model = resnet_50.ResNet50_Model(num_classes=num_plant_classes)
+resnet50_model = resnet_50_scratch_trained.ResNet50_Model(num_classes=num_plant_classes)
 
 # ------------ Train Model ------------
 history, duration = train_utils.scratch_train_model(resnet50_model,
